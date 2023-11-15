a4e5c51c6fefdd6bb3918608e21bcbad5915bd5a
David Rosental <david2005.rosental@gmail.com>
2023-11-15 14:56:48 +0000

    Fixed commandline inputs (I previously forgot about the key)

--- src/desrs.rs
+++ src/desrs.rs
@@ -1,2 +1,14 @@
 mod keygen;
 mod encode;
+
+pub fn encrypt(data: &[u8], key: u64) {
+    println!("[ENCRYPTING]");
+    dbg!(data);
+    dbg!(key);
+}
+
+pub fn decrypt(data: &[u8], key: u64) {
+    println!("[DECRYPTING]");
+    dbg!(data);
+    dbg!(key);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
 use std::{env, process};
+use desrs::desrs::{encrypt, decrypt};
+
+macro_rules! invalid_args {
+    ($extra:expr) => {
+        eprintln!("{}", $extra);
+        eprintln!("USAGE: desrs [ed] <data> <key>");
+        process::exit(1);           
+    };
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        eprintln!("USAGE: desrs [ed] <string>");
-        process::exit(1);
+    if args.len() != 4 {
+        invalid_args!("You need 4 arguments.");
     }
 
-    let mode = &args[1];
-    let data = &args[2];
-
-    if !(*mode).is_ascii() || !(*data).is_ascii() {
-        eprintln!("USAGE: desrs [ed] <string>");
-        process::exit(1);
+    if !args[1].is_ascii() || !args[2].is_ascii() || !args[3].is_ascii() {
+        invalid_args!("All arguments must be ASCII.");
     }
 
-    if (*mode).len() != 1 || (!(*mode).ends_with('e') && !(*mode).ends_with('d')) {
-        eprintln!("USAGE: desrs [ed] <string>");
-        process::exit(1);
+    if args[1].len() != 1 || (!args[1].ends_with('e') && !args[1].ends_with('d')) {
+        invalid_args!("[ed] argument must be either simply 'e' or 'd'");
+    }
+
+    // extract what should be the only character in the first argument
+    let mode = args[1].chars().next().unwrap();
+
+    let data = args[2].as_bytes();
+
+    let key_bytes = args[3].as_bytes();
+
+    if key_bytes.len() != 8 {
+        invalid_args!("Key must be 64 bits (8 ASCII characters)");
+    }
+
+    if mode == 'e' {
+        //encrypt(data, key);
+    } else if mode == 'd' {
+        //decrypt(data, key);
+    } else {
+        assert!(false);
     }
 }
