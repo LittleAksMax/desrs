d4874f752f15815a22f7c119112c2d49a5426d1c
David R <david2005.rosental@gmail.com>
2024-01-05 19:41:45 +0000

    Ready for encryption/decryption!

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,19 @@ macro_rules! invalid_args {
     };
 }
 
+macro_rules! key_from_bytes {
+    ($bytes:expr) => {
+        (($bytes[0] as u64) << 56) |
+        (($bytes[1] as u64) << 48) |
+        (($bytes[2] as u64) << 40) |
+        (($bytes[3] as u64) << 32) |
+        (($bytes[4] as u64) << 24) |
+        (($bytes[5] as u64) << 16) |
+        (($bytes[6] as u64) << 8) |
+        ($bytes[7] as u64)
+    };
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -35,10 +48,12 @@ fn main() {
         invalid_args!("Key must be 64 bits (8 ASCII characters)");
     }
 
+    let key: u64 = key_from_bytes!(key_bytes);
+
     if mode == 'e' {
-        //encrypt(data, key);
+        encrypt(data, key);
     } else if mode == 'd' {
-        //decrypt(data, key);
+        decrypt(data, key);
     } else {
         assert!(false);
     }
